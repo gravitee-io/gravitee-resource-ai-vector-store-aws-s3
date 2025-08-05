2ac65d9de133484ad676d390aaa197d3afc1312a
Derek Thompson <derek.thompson@graviteesource.com>
2025-08-05 18:46:40 -0400

    chore: replace redis log with aws s3 logo

--- src/main/java/io/gravitee/resource/ai/vector/store/aws/s3/configuration/AWSS3Configuration.java
+++ src/main/java/io/gravitee/resource/ai/vector/store/aws/s3/configuration/AWSS3Configuration.java
@@ -13,13 +13,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.gravitee.resource.ai.vector.store.redis.configuration;
+package io.gravitee.resource.ai.vector.store.aws.s3.configuration;
 
 /**
  * @author Rémi SULTAN (remi.sultan at graviteesource.com)
  * @author GraviteeSource Team
  */
-public record RedisConfiguration(
+public record AWSS3Configuration(
   String url,
   String username,
   String password,
